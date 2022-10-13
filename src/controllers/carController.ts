0566ef7343fbe9e6c854bf4c9ded7d32f3fed327
raphabarzotto <raphabarzotto@gmail.com>
2022-10-13 14:32:19 -0300

    feat: add getById in carController

--- src/controllers/carController.ts
+++ src/controllers/carController.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import CarService from '../services/carService';
 
-export default class CarController {
+export default class CarsController {
   constructor(private carService = new CarService()) {}
 
   public getAll = async (req: Request, res: Response, next: NextFunction) => {
@@ -13,6 +13,28 @@ export default class CarController {
       }
   
       return res.status(200).json(cars);
+    } catch (error) {
+      console.log(error);
+      return next(error);
+    }
+  };
+
+  public getById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      if (id.length !== 24) {
+        return res.status(400)
+          .json({ error: 'Id must have 24 hexadecimal characters' });
+      }
+
+      const carById = await this.carService.getById(id);
+
+      if (!carById) {
+        return res.status(404)
+          .json({ error: 'Object not found' });
+      }
+
+      return res.status(200).json(carById);
     } catch (error) {
       console.log(error);
       return next(error);
