289d2201b0fea1122dbb2752e14a7ad6f903bd8e
raphabarzotto <raphabarzotto@gmail.com>
2022-10-13 20:03:07 -0300

    feat: add create to carsModel

---
+++ src/models/carModel.ts
@@ -0,0 +1,22 @@
+import { Schema, model as createModel } from 'mongoose';
+import { ICar } from '../interfaces/ICar';
+
+const carSchema = new Schema<ICar>({
+  model: { type: String, required: true },
+  year: { type: Number, required: true },
+  color: { type: String, required: true },
+  buyValue: { type: Number, required: true },
+  doorsQty: { type: Number, required: true },
+  seatsQty: { type: Number, required: true },
+  status: { type: String, required: false },
+}, {
+  versionKey: false,
+});
+
+export default class CarsModel {
+  constructor(private carsModel = createModel<ICar>('cars', carSchema)) {}
+
+  public async create(newCar: ICar): Promise<ICar> {
+    return this.carsModel.create(newCar);
+  }
+}
