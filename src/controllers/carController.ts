bf3d3e6c91ce6bf4a5516f4c524fe206d590af1b
raphabarzotto <raphabarzotto@gmail.com>
2022-10-13 20:04:46 -0300

    feat: add create to carsController

---
+++ src/controllers/carController.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from 'express';
+import CarsService from '../services/carService';
+
+import testCarBody from '../helper/testCarBody';
+
+const error400Message = 'Id must have 24 hexadecimal characters';
+// const error404Message = 'Object not found';
+
+export default class CarsController {
+  constructor(private carsService = new CarsService()) {}
+
+  public create = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const carToCreate = req.body;
+
+      if (testCarBody(carToCreate)) {
+        return res.status(400).json({ error: error400Message });
+      }
+
+      const createdCar = await this.carsService.create(carToCreate);
+      if (!createdCar) {
+        return res.status(400).json({ error: 'Can not be possible to create a car' });
+      }
+  
+      return res.status(201).json(createdCar);
+    } catch (error) {
+      return next(error);
+    }
+  };
+}
