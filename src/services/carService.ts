9abe342151bb16a1cf0bcaf1782a93522149b3dd
raphabarzotto <raphabarzotto@gmail.com>
2022-10-13 14:32:06 -0300

    feat: add getById in carService

--- src/services/carService.ts
+++ src/services/carService.ts
@@ -1,9 +1,14 @@
+import { ICar } from '../interfaces/ICar';
 import CarModel from '../models/carModel';
 
 export default class CarService {
-  constructor(private carModel = new CarModel()) {}
+  constructor(private carsModel = new CarModel()) {}
 
   public async getAll() {
-    return this.carModel.getAll();
+    return this.carsModel.getAll();
+  }
+
+  public async getById(id: string) {
+    return this.carsModel.getById(id);
   }
 }
