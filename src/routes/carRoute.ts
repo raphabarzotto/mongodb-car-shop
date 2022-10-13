5662288de683f49bc401b8253c2c173de14851d4
raphabarzotto <raphabarzotto@gmail.com>
2022-10-13 14:06:07 -0300

    feat: create carRouter and add getAll to it

---
+++ src/routes/carRoute.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import CarModel from '../models/carModel';
+import CarService from '../services/carService';
+import CarController from '../controllers/carController';
+
+const CarRoute = Router();
+
+const car = new CarModel();
+const carService = new CarService(car);
+const carController = new CarController(carService);
+
+CarRoute.get('/', carController.getAll);
+
+export default CarRoute;
