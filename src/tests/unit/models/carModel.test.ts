7d0ef2e317a3ee4a149cc4eceb7862be1ec39a3b
raphabarzotto <raphabarzotto@gmail.com>
2022-10-13 23:35:14 -0300

    test: add carModel tests

---
+++ src/tests/unit/models/carModel.test.ts
@@ -0,0 +1,50 @@
+import * as sinon from 'sinon';
+import chai from 'chai';
+import { Model } from 'mongoose'
+import CarModel from '../../../models/carModel';
+import { carMock, carMockId } from '../../mocks/carMocks';
+
+const { expect } = chai;
+
+describe('Car Model tests', () => {
+  const carModel = new CarModel();
+  after(() => {sinon.restore()});
+
+  it('It is possible to create a car sussesfully', async () => {
+    sinon.stub(Model, 'create').resolves(carMockId);
+    const carCreated = await carModel.create(carMock);
+    expect(carCreated).to.be.eql(carMockId);
+  })
+
+  it('It is possible to get all cars sussesfully', async () => {
+    sinon.stub(Model, 'find').resolves([carMockId]);
+    const cars = await carModel.getAll();
+    expect(cars).to.be.eql([carMockId]);
+  })
+
+  it('It is possible to get a cars sussesfully by ID', async () => {
+    sinon.stub(Model, 'findById').resolves(carMockId);
+    const cars = await carModel.getById('6324b1f7e63a20956a30c009');
+    expect(cars).to.be.eql(carMockId);
+  })
+
+  it('It is possible to update a cars sussesfully by ID', async () => {
+    sinon.stub(Model, 'findByIdAndUpdate').resolves(carMockId);
+    try {
+      const car = await carModel.update('6324b1f7e63a20956a30c009', carMock);
+      expect(car).to.be.eql(carMockId);
+    } catch (error: any) {
+      expect(error).to.exist;
+    }
+  })
+
+  it('It is possible to delete a cars sussesfully by ID', async () => {
+    sinon.stub(Model, 'findByIdAndDelete').resolves();
+    try {
+      const carDelete = await carModel.delete('6324b1f7e63a20956a30c009');
+      expect(carDelete).to.be.undefined;
+    } catch (error: any) {
+      expect(error).to.exist;
+    }
+  })
+});
