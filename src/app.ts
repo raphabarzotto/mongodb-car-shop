b89ef18a1ec08c89477af959fa13d8b99548aadb
raphabarzotto <raphabarzotto@gmail.com>
2022-10-13 23:10:21 -0300

    fix: space in motorcycles route

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import carRoute from './routes/carRoute';
-import MotorcycleRoute from './routes/motorcycleRoute';
+import motorcycleRoute from './routes/motorcycleRoute';
 
 const app = express();
 
 app.use(express.json());
 app.use('/cars', carRoute);
-app.use('/motorcycles ', MotorcycleRoute);
+app.use('/motorcycles', motorcycleRoute);
 
 export default app;
