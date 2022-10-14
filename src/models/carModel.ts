1af6c3d6b08ec7bbedcdc4dcdecc5a4682972c53
raphabarzotto <raphabarzotto@gmail.com>
2022-10-13 22:01:01 -0300

    feat: add update and delete in carModel

--- src/models/carModel.ts
+++ src/models/carModel.ts
@@ -27,4 +27,17 @@ export default class CarsModel {
   public async getById(id: string) {
     return this.carsModel.findById(id);
   }
+
+  public async update(id: string, carData: object): Promise<ICar | null> {
+    const car = await this.carsModel.findOneAndUpdate(
+      { _id: id },
+      { ...carData },
+    );
+    return car;
+  }
+
+  public async delete(id: string): Promise<ICar | null> {
+    const car = await this.carsModel.findOneAndDelete({ _id: id });
+    return car;
+  }
 }
