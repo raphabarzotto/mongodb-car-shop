f05eb24d9b8586318d7f25ec5953b43815e8d31c
raphabarzotto <raphabarzotto@gmail.com>
2022-10-13 22:40:11 -0300

    feat: add motorcycle router to app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import carRoute from './routes/carRoute';
+import MotorcycleRoute from './routes/motorcycleRoute';
 
 const app = express();
 
 app.use(express.json());
 app.use('/cars', carRoute);
+app.use('/motorcycles ', MotorcycleRoute);
 
 export default app;
