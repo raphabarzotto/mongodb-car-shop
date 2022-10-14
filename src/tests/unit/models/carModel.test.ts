4e589ec4a7446c8067abfcc7b32dbbe38f6fe0b1
raphabarzotto <raphabarzotto@gmail.com>
2022-10-14 00:07:32 -0300

    fix: typo

--- src/tests/unit/models/carModel.test.ts
+++ src/tests/unit/models/carModel.test.ts
@@ -10,25 +10,25 @@ describe('Car Model tests', () => {
   const carModel = new CarModel();
   after(() => {sinon.restore()});
 
-  it('It is possible to create a car sussesfully', async () => {
+  it('It is possible to create a car successfully', async () => {
     sinon.stub(Model, 'create').resolves(carMockId);
     const carCreated = await carModel.create(carMock);
     expect(carCreated).to.be.eql(carMockId);
   })
 
-  it('It is possible to get all cars sussesfully', async () => {
+  it('It is possible to get all cars successfully', async () => {
     sinon.stub(Model, 'find').resolves([carMockId]);
     const cars = await carModel.getAll();
     expect(cars).to.be.eql([carMockId]);
   })
 
-  it('It is possible to get a cars sussesfully by ID', async () => {
+  it('It is possible to get a cars successfully by ID', async () => {
     sinon.stub(Model, 'findById').resolves(carMockId);
     const cars = await carModel.getById('6324b1f7e63a20956a30c009');
     expect(cars).to.be.eql(carMockId);
   })
 
-  it('It is possible to update a cars sussesfully by ID', async () => {
+  it('It is possible to update a car successfully by ID', async () => {
     sinon.stub(Model, 'findByIdAndUpdate').resolves(carMockId);
     try {
       const car = await carModel.update('6324b1f7e63a20956a30c009', carMock);
@@ -38,7 +38,7 @@ describe('Car Model tests', () => {
     }
   })
 
-  it('It is possible to delete a cars sussesfully by ID', async () => {
+  it('It is possible to delete a car successfully by ID', async () => {
     sinon.stub(Model, 'findByIdAndDelete').resolves();
     try {
       const carDelete = await carModel.delete('6324b1f7e63a20956a30c009');
