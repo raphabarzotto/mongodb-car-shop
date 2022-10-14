71bf8efaa61490bf3f02270f45ba25737e5f3861
raphabarzotto <raphabarzotto@gmail.com>
2022-10-13 23:09:15 -0300

    fix: lint motorcyclebody helper

--- src/helper/testMotorcycleBody.ts
+++ src/helper/testMotorcycleBody.ts
@@ -1,22 +1,21 @@
 import { IMotorcycle } from '../interfaces/IMotorcycle';
 
-export default function testCarBody(moto: IMotorcycle) {
+export default function testMotorcycleBody(moto: IMotorcycle) {
   const lenTest: boolean = Object.keys(moto).length === 0;
-  const categoryTest: boolean = moto.category !== 'Street' && moto.category !== 'Custom' && moto.category !== 'Trail';
-  const engineTest: boolean = moto.engineCapacity < 0 || moto.engineCapacity > 2500;
-  const modelTest = !moto.model;
-  const yearTest = !moto.year;
-  const colorTest = !moto.color;
-  const buyTest = !moto.buyValue;
+  const categoryTest: boolean = moto.category !== 'Street' 
+    && moto.category !== 'Custom' && moto.category !== 'Trail';
+  const engineTest: boolean = moto.engineCapacity < 1 || moto.engineCapacity > 2500;
 
   const arraytest = [
     lenTest,
     categoryTest,
     engineTest,
-    modelTest,
-    yearTest,
-    colorTest,
-    buyTest,
+    !moto.model,
+    !moto.year,
+    !moto.color,
+    !moto.buyValue,
+    !moto.category,
+    !moto.engineCapacity,
   ];
 
   return arraytest.includes(true);
